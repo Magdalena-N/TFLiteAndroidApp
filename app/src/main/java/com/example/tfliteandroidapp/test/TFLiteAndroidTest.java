f20996783ca08a6372fe657b3a1278a834adbce3
Wojciech Drewek <drewekwojciech@gmail.com>
2020-08-20 22:01:40 +0200

    Create 'TFLiteAndroidTest' class
    'TFLiteAndroidTest' class will run all tests. Function
    'initInterpreter' loads model and sets Interpreter's options.

---
+++ app/src/main/java/com/example/tfliteandroidapp/test/TFLiteAndroidTest.java
@@ -0,0 +1,77 @@
+package com.example.tfliteandroidapp.test;
+
+import android.app.Activity;
+
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.gpu.GpuDelegate;
+import org.tensorflow.lite.nnapi.NnApiDelegate;
+import org.tensorflow.lite.support.common.FileUtil;
+
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+
+public class TFLiteAndroidTest {
+
+    public enum Device {
+        CPU,
+        NNAPI,
+        GPU
+    }
+
+    /** TFLite model loaded into memory */
+    private MappedByteBuffer tfliteModel;
+
+    /** Interpreter which runs model inference with Tensorflow Lite. */
+    private Interpreter interpreter;
+
+    /** Options of interpreter */
+    private final Interpreter.Options tfliteOptions;
+
+    /** Activity in which the test is performed */
+    private Activity activity;
+
+    /** Current device used for executing classification */
+    private Device currentDevice;
+
+    /** Optional GPU delegate  */
+    private GpuDelegate gpuDelegate;
+
+    /** Optional NNAPI delegate */
+    private NnApiDelegate nnApiDelegate;
+
+    public TFLiteAndroidTest(Activity pA)
+    {
+        activity = pA;
+        currentDevice = Device.CPU;
+        gpuDelegate = null;
+        nnApiDelegate = null;
+        tfliteOptions = new Interpreter.Options();
+    }
+
+    /**
+     * Initiates interpreter, loads model, optionally adds delegate
+     *
+     * @param model Name of .tflite file
+     */
+    private void initInterpreter(String model)
+    {
+        switch (currentDevice) {
+            case GPU:
+                gpuDelegate = new GpuDelegate();
+                tfliteOptions.addDelegate(gpuDelegate);
+                break;
+            case NNAPI:
+                nnApiDelegate = new NnApiDelegate();
+                tfliteOptions.addDelegate(nnApiDelegate);
+                break;
+            case CPU:
+                break;
+        }
+        try {
+            tfliteModel = FileUtil.loadMappedFile(activity, model);
+            interpreter = new Interpreter(tfliteModel, tfliteOptions);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
