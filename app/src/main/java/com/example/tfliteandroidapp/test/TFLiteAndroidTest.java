bc762ae63cd692ecb8c95b8f698c9bb8ad4afec5
Wojciech Drewek <drewekwojciech@gmail.com>
2020-08-25 21:12:28 +0200

    Initialization of input and output buffers.

--- app/src/main/java/com/example/tfliteandroidapp/test/TFLiteAndroidTest.java
+++ app/src/main/java/com/example/tfliteandroidapp/test/TFLiteAndroidTest.java
@@ -2,10 +2,15 @@ package com.example.tfliteandroidapp.test;
 
 import android.app.Activity;
 
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.gpu.GpuDelegate;
 import org.tensorflow.lite.nnapi.NnApiDelegate;
 import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
@@ -41,6 +46,20 @@ public class TFLiteAndroidTest {
     /** Optional NNAPI delegate */
     private NnApiDelegate nnApiDelegate;
 
+    /** Input image TensorBuffer of current interpreter*/
+    private TensorImage inputImageBuffer;
+
+    /** Output probability TensorBuffer of current interpreter*/
+    private TensorBuffer outputProbabilityBuffer;
+
+    /** Processer to apply post processing of the output probability */
+    private TensorProcessor probabilityProcessor;
+
+    /** Shape of input image */
+    private int imageSizeY, imageSizeX;
+
+    private boolean isQuantized;
+
     public TFLiteAndroidTest(Activity pA)
     {
         activity = pA;
@@ -75,6 +94,41 @@ public class TFLiteAndroidTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Reads image shape, image data type from InputTensor and probability shape
+     * and probability data type from OutputTensor. Initiates inputImageBuffer and
+     * outputProbabilityBuffer.
+     *
+     * Initiates probabilityProcessor with mean equal to 0 and std equal to 255 when mobilenet
+     * model is quantized because such model needs additional dequantization to the output
+     * probability. When model is not quantized then std is set to 1.
+     */
+    private void prepareBuffers()
+    {
+        int[] imageShape;
+        int[] probabilityShape;
+        DataType imageDataType;
+        DataType probabilityDataType;
+        float probSTD;
+
+        imageShape = interpreter.getInputTensor(0).shape();
+        imageSizeY = imageShape[1];
+        imageSizeX = imageShape[2];
+        imageDataType = interpreter.getInputTensor(0).dataType();
+        probabilityShape = interpreter.getOutputTensor(0).shape();
+        probabilityDataType = interpreter.getOutputTensor(0).dataType();
+
+        inputImageBuffer = new TensorImage(imageDataType);
+        outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+
+        if (isQuantized)
+            probSTD = 255;
+        else
+            probSTD = 1;
+
+        probabilityProcessor = new TensorProcessor.Builder().add(new NormalizeOp(0.0f, probSTD)).build();
     }
 
     /**
